ad0f7bd02d9a2b1910270a3158ff3583f318d8ba
TRUCKADVISOR-PORTUGAL <sales@truckadvisorportugal.com>
2025-08-01 14:50:48 +0100

    Adicionar output: export ao next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,7 @@
-const nextConfig = { output: 'export' }; module.exports = nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+};
+
+module.exports = nextConfig;
+
